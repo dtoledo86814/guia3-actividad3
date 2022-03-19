a15e371e6ae1c45398bbafb782fdeaea405d9314
dtoledo86814 <99734755+dtoledo86814@users.noreply.github.com>
2022-03-19 12:50:27 -0500

    Add files via upload

---
+++ EvaluadorPostfijo/EvaluadorPostfijo/src/universidadean/desarrollosw/postfijo/EvaluadorPostfijo.java
@@ -0,0 +1,72 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad Ean (Bogotá - Colombia)
+ * Departamento de Tecnologías de la Información y Comunicaciones
+ * Licenciado bajo el esquema Academic Free License version 2.1
+ * <p>
+ * Proyecto Evaluador de Expresiones Postfijas
+ * Fecha: Febrero 2021
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package universidadean.desarrollosw.postfijo;
+
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Esta clase representa una clase que evalúa expresiones en notación polaca o
+ * postfija. Por ejemplo: 4 5 +
+ */
+public class EvaluadorPostfijo {
+
+    /**
+     * Realiza la evaluación de la expresión postfijo utilizando una pila
+     * @param expresion una lista de elementos con números u operadores
+     * @return el resultado de la evaluación de la expresión.
+     */
+    static int evaluarPostFija(List<String> expresion) {
+        Stack<Integer> pila = new Stack<>();
+        int num1;
+        int num2;
+        for(int i =0; i<expresion.size();i++){
+            String ex = expresion.get(i);
+            System.out.println("ok for"+ ex);
+            if(ex=="+") {
+                System.out.println(ex);
+                num1 =pila.pop();
+                num2 =pila.pop();
+                pila.push(num2+num1);
+                System.out.println(pila.pop());
+            }
+            else {
+                System.out.println(ex);
+                pila.push(Integer.valueOf(ex));
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Programa principal
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("> ");
+        String linea = teclado.nextLine();
+
+        try {
+            List<String> expresion = Token.dividir(linea);
+            System.out.println(evaluarPostFija(expresion));
+        }
+        catch (Exception e) {
+            System.err.printf("Error grave en la expresión: %s", e.getMessage());
+        }
+
+    }
+}
